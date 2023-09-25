89e1f0a5b2fb47489b3eb805616f598c332d222b
Sergey Lyulchenko <solo300m@yandex.ru>
2023-09-25 20:43:11 +0400

    task_2

--- task_2.cpp
+++ task_2.cpp
@@ -3,4 +3,21 @@
 using namespace std;
 int main()
 {
+    string path = "C:\\develop\\txt\\Harry_Potter\\Harry_Potter.txt";
+    ifstream book;
+    char str[300];
+    book.open(path, std::ios::binary);
+    int count = 0;
+    char next;
+    cout<<"Для выхода из режима чтения нажмите Q";
+    while (next != 'Q')
+    {
+        book.read(str, sizeof(str));
+        str[299] = 0;
+        cout << str << endl;
+        int tmp = book.tellg();
+        book.seekg(tmp - 1);
+        cin >> next;
+    }
+    book.close();
 }
